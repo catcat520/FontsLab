bd21a634e3a0ac0a8f454c2fc06371f1f77e0b99
catcat520 <catcat520@users.noreply.github.com>
2016-08-25 00:57:00 +0800

    Update CharSet.js

--- CharSet.js
+++ CharSet.js
@@ -3,4 +3,5 @@
 简<->日 : MS936/GB2312/GBK<->EUC-JP/CP20932 : 卜悶
 简<->韩 : MS936/GB2312/GBK<->EUC-KR/CP51949 : 芥竟
 简<->繁 : MS936/GB2312/GBK<->MS950/Big5 : 冼极
-简<->越 : MS936/GB2312/GBK<->Windows1258 : ËÎ̀å
+简<->越 : MS936/GB2312/GBK<->Windows-1258 : ËÎ̀å
+简<->泰 : MS936/GB2312/GBK<->Windows-874/ISO-8859-11 : หฮฬๅ
