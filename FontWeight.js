fb136c58bf85e4550d2503489a7b732ea4c41257
catcat520 <catcat520@users.noreply.github.com>
2016-09-17 13:54:32 +0800

    Update and rename FontWeight.css to FontWeight.js

---
+++ FontWeight.js
@@ -0,0 +1,9 @@
+/* 100:Thin/Hairline */
+/* 200:Extra Light/Ultra Light */
+/* 300:Light */
+/* 400:Normal/Regular/Roman */
+/* 500:Medium */
+/* 600:Semibold/Demibold */
+/* 700:Bold */
+/* 800:Extrabold/Ultrabold */
+/* 900:Heavy/Black */
